a25ebd44b6b6a9aa543de1fff16326c4afb5cfe8
Raj Singh <rajsingh.rsr9824@gmail.com>
2024-03-01 11:06:24 +0545

    add middleware to register route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,15 +1,31 @@
-import { Router } from "express";
-import { registerUser} from "../controllers/user.controller.js";
+import Router from "express";
+import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+
 
 // this is "userRouter = router" , as we have exported in default we can use another name to import.  in our case we have imported router as userRouter
 const router = Router()
 
-router.route("/register").post(registerUser)
+
+// router.route("/register").post(registerUser)
+
+
+router.route("/register").post(
+    
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser)
 
 
 
 
 
-
-
-export default router
+export default router 

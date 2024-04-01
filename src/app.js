725a7526c65314412404520d005d9ccc0d59308e
Raj-venom <rajsingh.rsr9824@gmail.com>
2024-04-01 19:20:23 +0545

    Add routes for comment

--- src/app.js
+++ src/app.js
@@ -2,9 +2,6 @@ import express from "express"
 import cookieParser from "cookie-parser"
 import cors from "cors"
 
-
-
-
 const app = express()
 
 app.use(cors({
@@ -33,8 +30,7 @@ import userRouter from "./routes/user.routes.js"
 import healthcheckRouter from "./routes/healthcheck.routes.js"
 import tweetRouter from "./routes/tweet.routes.js"
 import videoRouter from "./routes/video.routes.js"
-
-
+import commentRouter from "./routes/comment.routes.js"
 
 
 //routes declaration
@@ -42,7 +38,7 @@ app.use("/api/v1/healthcheck", healthcheckRouter)
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/tweets", tweetRouter)
 app.use("/api/v1/videos", videoRouter)
-
+app.use("/api/v1/comments", commentRouter)
 
 
 export default app 

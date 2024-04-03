9e4566b7d5b2a8d58595f98d0d9ffb529b223bb7
Raj-venom <rajsingh.rsr9824@gmail.com>
2024-04-04 00:21:55 +0545

    Fix typo

--- src/models/playlist.model.js
+++ src/models/playlist.model.js
@@ -24,4 +24,4 @@ const playlistSchema = new Schema(
     },
     { timestamps: true })
 
-export const PlayList = mongoose.model("PlayList", playlistSchema)
+export const Playlist = mongoose.model("Playlist", playlistSchema)
